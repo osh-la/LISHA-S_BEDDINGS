6ea63f2f9e57470d4e3127b4adcb54643b70886f
abimiku oshela <abimikuoshela@gmail.com>
2026-08-21 11:59:56 +0100

    cat

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -11,7 +11,7 @@ export default function ShopPage() {
   const { products } = useProducts();
   const { categorySlug } = useParams();
 
-  const categories = ["All", "BEDSPREAD", "DUVETS", "PILLOWS", "CURTAINS", "THROWS","ACCESSORIES"];
+  const categories = ["All","ACCESSORIES", "BEDSPREAD", "DUVETS", "PILLOWS", "CURTAINS", "THROWS"];
   const [filter, setFilter] = useState("All");
 
   const containerRef = useRef(null);
